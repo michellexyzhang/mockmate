c46faf5ce247d9a3e639f0cd05fe1afadf1207cf
mzhang055 <mzhang055@gmail.com>
2025-06-19 20:11:47 -0400

    landing setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+        <h1 className="text-9xl font-extrabold font-[family-name:var(--font-pencil)]">
+          mockate
+        </h1>
+        <div className="mt-12">
+          <button className="px-8 py-3 text-lg font-semibold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+            Start
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+}
