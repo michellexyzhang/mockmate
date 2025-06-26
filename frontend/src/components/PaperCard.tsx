a1840592cb09f75c166b8a92289d0a4c59a343a7
mzhang055 <mzhang055@gmail.com>
2025-06-26 13:31:08 -0400

    style: change card sizes

--- frontend/src/components/PaperCard.tsx
+++ frontend/src/components/PaperCard.tsx
@@ -17,14 +17,14 @@ const PaperCard = ({ rotation, badgeColor, badgeIcon, className = "" }: PaperCar
       className={className}
     >
       <div
-        className={`relative bg-white rounded-xl shadow-xl w-32 h-40 p-2 sm:w-66 sm:h-84 sm:p-4 flex flex-col justify-end rotate-[${rotation}deg]`}
+        className={`relative bg-white rounded-xl shadow-xl w-16 h-20 p-1 md:w-24 md:h-28 md:p-2 lg:w-32 lg:h-40 lg:p-3 xl:w-44 xl:h-56 xl:p-4 flex flex-col justify-end rotate-[${rotation}deg]`}
       >
-        <div className="absolute top-3 left-3 w-12 h-1 bg-gray-200 rounded sm:top-6 sm:left-6 sm:w-20 sm:h-2"></div>
-        <div className="absolute top-6 left-3 w-10 h-1 bg-gray-100 rounded sm:top-12 sm:left-6 sm:w-16 sm:h-2"></div>
-        <div className="absolute top-9 left-3 w-16 h-1 bg-gray-100 rounded sm:top-16 sm:left-6 sm:w-24 sm:h-2"></div>
+        <div className="absolute top-2 left-2 w-8 h-1 bg-gray-200 rounded md:top-3 md:left-3 md:w-12 md:h-1 lg:top-4 lg:left-4 lg:w-16 lg:h-1 xl:top-6 xl:left-6 xl:w-20 xl:h-2"></div>
+        <div className="absolute top-4 left-2 w-6 h-1 bg-gray-100 rounded md:top-6 md:left-3 md:w-10 md:h-1 lg:top-8 lg:left-4 lg:w-12 lg:h-1 xl:top-12 xl:left-6 xl:w-16 xl:h-2"></div>
+        <div className="absolute top-6 left-2 w-10 h-1 bg-gray-100 rounded md:top-9 md:left-3 md:w-14 md:h-1 lg:top-12 lg:left-4 lg:w-20 lg:h-1 xl:top-16 xl:left-6 xl:w-24 xl:h-2"></div>
 
-        <div className={`absolute bottom-2 right-2 ${badgeColor} w-6 h-6 flex items-center justify-center rounded-full shadow-md border-2 border-white sm:bottom-3 sm:right-3 sm:w-8 sm:h-8`}>
-          <span className="text-white text-lg sm:text-xl">{badgeIcon}</span>
+        <div className={`absolute bottom-1 right-1 ${badgeColor} w-4 h-4 flex items-center justify-center rounded-full shadow-md border-2 border-white md:bottom-2 md:right-2 md:w-6 md:h-6 lg:bottom-3 lg:right-3 lg:w-8 lg:h-8`}>
+          <span className="text-white text-xs md:text-lg lg:text-xl">{badgeIcon}</span>
         </div>
       </div>
     </motion.div>
